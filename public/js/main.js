eb97b05b120f39fbafa02912fa6b9d5573b79a97
Bryce <bryceandy@rocketmail.com>
2018-05-31 22:32:34 +0300

    css and js

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+$("#services").click(function(){
+    TweenMax.to(window, 1.5, {scrollTo:"#eservices"});
+});
+
+$("#introButton").click(function(){
+    TweenMax.to(window, 1.5, {scrollTo:"#eservices"});
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,6 +63,10 @@
         <button id="introButton"><span class="introbtntxt">SEE MORE</span></button>
     </div>
 
+    <div id="eservices">
+
+    </div>
+
 </div>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
